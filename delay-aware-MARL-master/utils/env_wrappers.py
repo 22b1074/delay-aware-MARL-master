19109409748ff707d30ab8ca324000eb71f0efea
22b1074 <130027346+22b1074@users.noreply.github.com>
2025-10-26 16:24:54 +0530

    Update env_wrappers.py

--- delay-aware-MARL-master/utils/env_wrappers.py
+++ delay-aware-MARL-master/utils/env_wrappers.py
@@ -121,10 +121,10 @@ class DummyVecEnv(VecEnv):
         self.actions = None
         return np.array(obs), np.array(rews), np.array(dones), infos
 
-    def reset(self, **kwargs):
-        # keep per-env and per-agent observations as lists
-        results = [env.reset(**kwargs) for env in self.envs]  # each is list of obs per agent
-        return results
+     def reset(self):        
+        results = [env.reset() for env in self.envs]
+        return np.array(results)
+
 
     def close(self):
         return
