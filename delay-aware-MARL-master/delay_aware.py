103100f7f16743851086d20c23e1812f6f2605d8
22b1074 <130027346+22b1074@users.noreply.github.com>
2025-10-26 22:06:39 +0530

    Update delay_aware.py

--- delay-aware-MARL-master/delay_aware.py
+++ delay-aware-MARL-master/delay_aware.py
@@ -75,11 +75,11 @@ def run(config):
                                   tau=config.tau,
                                   lr=config.lr,
                                   hidden_dim=config.hidden_dim,
-                                  delay_step = 2)
+                                  delay_step = 1)
     print(f"[DEBUG] MADDPG initialized with {maddpg.nagents} agents")
     for i, agent in enumerate(maddpg.agents):
         print(f"[DEBUG] Agent {i} policy input dim: {agent.policy.in_fn}")
-    delay_step = 2
+    delay_step = 1
     #base_env used
     replay_buffer = ReplayBuffer(
         config.buffer_length, 
